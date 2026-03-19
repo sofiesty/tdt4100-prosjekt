c98be848c4b539e9c18f43ae34bbd670441cbbdd
sofiesty <sofie04@gmail.com>
2026-03-19 10:55:34 +0100

    Started on Controller class

--- src/main/java/moviereview/fxui/model/MovieReviewController.java
+++ src/main/java/moviereview/fxui/model/MovieReviewController.java
@@ -1,5 +1,60 @@
 package moviereview.fxui.model;
 
 public class MovieReviewController {
+    @FXML private TableView<Movies> movieTableView;
+    @FXML private TableColumn<Movies, String>  movieTitleColumn;
+    @FXML private TableColumn<Movies, Double>  averageRatingColumn;
+    @FXML private TableColumn<Movies, Integer> reviewCountColumn;
+
+    @FXML private TextField  movieTitleField;
+    @FXML private TextField  usernameField;
+    @FXML private Spinner<Integer> ratingSpinner;
+    @FXML private DatePicker reviewDatePicker;
+
+    @FXML private Button addReviewButton;
+    @FXML private Button sortByRatingButton;
+    @FXML private Label  statusLabel;
+    @FXML private Label  bottomStatusLabel;
+
+    @FXML
+    public void initialize() {
+        movieTitleColumn.setCellValueFactory(
+            new PropertyValueFactory<>("title"));        
+        averageRatingColumn.setCellValueFactory(
+            new PropertyValueFactory<>("averageRating")); 
+        reviewCountColumn.setCellValueFactory(
+            new PropertyValueFactory<>("reviewCount"));  
+    }
+
+    @FXML
+    private void handleAddReview() {
+        String title = movieTitleField.getText();
+        String username = usernameField.getText();
+        Date date = reviewDatePicker.getValue();
+        Integer score = ratingSpinner.getValue();
+
+        if (title.isBlank() || username.isBlank() || date == null) {
+        statusLabel.setText("Please fill in all fields.");
+        return;
+        }
+
+        MovieReviewEntry movieReview = new MovieReviewEntry(title, score, date, username);
+        Movies.addMovie(movieReview);
+
+        movieTableView.setItems(99999999999999999999999);
+        statusLabel.setText("");
+        bottomStatusLabel.setText("Review added for: " + title);
     
+        // Tømmer feltene
+        movieTitleField.clear();
+        usernameField.clear();
+        reviewDatePicker.setValue(null);
+        ratingSpinner.getValueFactory().setValue(3);
+    }
+
+    @FXML
+    private void handleSortByRating() {
+
+    }
+
 }
