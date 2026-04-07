b8ca90b8d6194463f9e09f6736cc43bb6882a7e6
Celine Hagen <celinehagen@dhcp-10-24-41-49.wlan.ntnu.no>
2026-04-07 19:00:40 +0200

    Lagde en til test, en for kalkulator

--- src/test/java/test/MovieReviewTest.java
+++ src/test/java/test/MovieReviewTest.java
@@ -1,13 +1,16 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 
+import moviereview.model.MovieReviewCalculator;
 import moviereview.model.MovieReviewEntry;
 import moviereview.model.Movies;
+import moviereview.*;
 import java.util.ArrayList;
 
 public class MovieReviewTest {
@@ -26,5 +29,19 @@ public class MovieReviewTest {
         assertEquals(LocalDate.of(2026, 4, 7), test.get(0).getDate());
         assertEquals("celinehag", test.get(0).getUsername());
 
+    }
+
+    @Test
+    public void testgetAverageScore() {
+        Movies movies = new Movies();
+        MovieReviewCalculator calculator = new MovieReviewCalculator(movies);
+
+        MovieReviewEntry movie = new MovieReviewEntry("Pingu", 5, LocalDate.of(2026, 4, 7), "celinehag");
+        MovieReviewEntry movie2 = new MovieReviewEntry("Pingu", 3, LocalDate.of(2026, 4, 7), "celinehag");
+        
+        movies.addMovie(movie);
+        movies.addMovie(movie2);
+
+        assertEquals(4, calculator.avgScore("Pingu")); 
     }
 }
